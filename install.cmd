f2912e58ee4ccb54622d29c265fcc9088111e55b
Erik Araujo <ea.sub@icloud.com>
2024-06-22 08:58:47 -0300

    Updates install path for cs2

--- install.cmd
+++ install.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
 
-SET csgo_install_path=%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike Global Offensive
+SET csgo_install_path=%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike Global Offensive\game
 
 XCOPY %~dp0cfg\* "%csgo_install_path%\csgo\cfg" /Y /F
 XCOPY %~dp0resource\* "%csgo_install_path%\csgo\resource" /Y /F
